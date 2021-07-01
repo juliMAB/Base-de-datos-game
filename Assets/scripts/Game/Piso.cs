54316863ac691b25b48c4035f663616222c5933b
juliMAB <julian6aguirre@hotmail.com>
2021-07-01 03:38:13 -0300

    better pisos

--- Assets/scripts/Game/Piso.cs
+++ Assets/scripts/Game/Piso.cs
@@ -11,20 +11,27 @@ public class Piso : MonoBehaviour
     private MeshRenderer mR;
     private BoxCollider bC;
     [Range(2.0f, 6.0f)]
-    public float timeToLife;
+    [SerializeField] private float timeToLife;
     [Range(2.0f, 6.0f)]
-    public float timeToRespawn;
-    public Color c1;
-    public Color c2;
-    public enum State
+    [SerializeField] private float timeToRespawn;
+    [Range(2.0f, 6.0f)]
+    [SerializeField] private float dying;
+    [Range(2.0f, 6.0f)]
+    [SerializeField] private float timeDie;
+    [SerializeField] private Color c1;
+    [SerializeField] private Color c2;
+    [SerializeField]
+    private enum State
     {
         iddle,
         Muriendo,
+        Muerto,
         Respawneando,
+
         last
     }
 
-    private State actualState;
+    [SerializeField] private State actualState;
 
     void Start()
     {
@@ -32,47 +39,58 @@ public class Piso : MonoBehaviour
         mR.material.color = c1;
         bC = GetComponent<BoxCollider>();
         timeT = Random.Range(0, 20);
-        actualState = (State)Random.Range(0, 3);
-        if (actualState==State.Respawneando)
-        {
-            mR.material.color = new Color(mR.material.color.r, mR.material.color.g, mR.material.color.b, 0);
-        }
+        actualState = State.iddle;
     }
     
     void Update()
     {
         switch (actualState)
         {
+            case State.iddle:
+                timeT -= Time.deltaTime;
+                if (timeT > 0)
+                    return;
+                actualState++;
+                timeT = dying;
+
+                break;
             case State.Muriendo:
                 timeT -= Time.deltaTime;
-                aux = timeT / timeToLife;
+                aux = timeT / dying;
                 mR.material.color = new Color(mR.material.color.r, mR.material.color.g, mR.material.color.b, aux);
-                
-                if (timeT <= 0)
-                {
-                    actualState = State.Respawneando;
-                    timeT = timeToRespawn;
-                    mR.material.color = c2;
-                    mR.material.color =new Color(mR.material.color.r, mR.material.color.g, mR.material.color.b, aux);
-                    bC.enabled = false;
-                }
+
+                if (timeT > 0)
+                    return;
+                actualState++;
+                timeT = timeDie;
+                mR.material.color = c2;
+                mR.material.color = new Color(mR.material.color.r, mR.material.color.g, mR.material.color.b, 0);
+                bC.enabled = false;
+                break;
+            case State.Muerto:
+                timeT -= Time.deltaTime;
+                if (timeT > 0)
+                    return;
+                actualState++;
+                timeT = timeToRespawn;
+
                 break;
             case State.Respawneando:
                 timeT -= Time.deltaTime;
-                aux = (((timeT / timeToLife)*-1) +1);
-                if (aux<0)
-                {
-                    aux = 0;
-                }
+                aux = ((timeT / timeToLife)-1)*-0.999f;
+                print(aux);
                 mR.material.color = new Color(mR.material.color.r, mR.material.color.g, mR.material.color.b, aux);
-                if (timeT < 0)
-                {
+
+                if (timeT > 0)
+                    return;
                     mR.material.color = c1;
-                    mR.material.color = c1;
-                    actualState = State.Muriendo;
+                    actualState++ ;
                     timeT = timeToLife;
                     bC.enabled = true;
-                }
+                
+                break;
+            case State.last:
+                actualState=0;
                 break;
             default:
                 break;
